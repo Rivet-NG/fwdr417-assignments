9f6684f44299a240f0dd4ce6d3e347db0d34577e
Rivet-NG <43015084+Rivet-NG@users.noreply.github.com>
2023-05-19 19:55:00 -0400

    Homework 4 added!
    All Homework 4 files included.

---
+++ Homework 04/starter_code/level-three.js
@@ -0,0 +1,62 @@
+/*
+ * DOTS: Level Three
+ *
+ */
+let score = 0;
+let arenaEl = document.querySelector('.js-arena');
+let scoreEl = document.querySelector('.js-score');
+
+function declareWinner() {
+  document.body.classList.add('game-over');
+}
+  
+let ballList = arenaEl.querySelectorAll('.js-ball');
+
+let twentyPoints = Number(ballList[0].dataset.increment);
+let fivePoints = Number(ballList[1].dataset.increment);
+let tenPoints = Number(ballList[2].dataset.increment);
+  
+let counterOne = 0;
+let counterTwo = 0;
+let counterThree = 0;
+
+ballList[0].onclick = function() {
+  counterOne = score + twentyPoints;
+  if (counterOne >= 100) {
+    scoreEl.innerHTML = "0" + counterOne;
+    declareWinner();
+  }
+  else {
+    score = counterOne;
+    scoreEl.innerHTML = "00" + score;
+  }
+};
+  
+ballList[1].onclick = function() {
+  counterTwo = score + fivePoints;
+  if (counterTwo >= 100) {
+    scoreEl.innerHTML = "0" + counterTwo;
+    declareWinner();
+  }
+  else {
+    score = counterTwo;
+    if (score < 10) {
+      scoreEl.innerHTML = "000" + score;
+    }
+    else {
+      scoreEl.innerHTML = "00" + score;
+    }
+  }
+};
+  
+ballList[2].onclick = function() {
+  counterThree = score + tenPoints;
+  if (counterThree >= 100) {
+    scoreEl.innerHTML = "0" + counterThree;
+    declareWinner();
+  }
+  else {
+    score = counterThree;
+    scoreEl.innerHTML = "00" + score;
+  }
+};
